3f1fe86db3dcee0a5ba5727f116ca61b31498199
medvedicos <41338299+medvedicos@users.noreply.github.com>
2025-11-30 13:12:27 +0300

    Update install_misa.sh

--- install_misa.sh
+++ install_misa.sh
@@ -1,1 +1,3 @@
 curl -f https://raw.githubusercontent.com/medvedicos/Install_MiSa/refs/heads/main/banner --output /etc/banner
+
+curl -f https://raw.githubusercontent.com/medvedicos/Install_MiSa/refs/heads/main/hosts --output /etc/hosts
